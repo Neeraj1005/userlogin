62457cef58c745d3cd2fba811b7266f586128998
Neeraj1005 <neeraj.tangariya123@gmail.com>
2019-09-22 11:50:36 +0530

    Update jslogin.php

--- jslogin.php
+++ jslogin.php
@@ -1,12 +1,21 @@
 <?php 
 require_once('config.php');
-echo 'hello form js';
-$sql ="SELECT * FROM users";
+
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$sql ="SELECT * FROM users WHERE email = ? AND password = ? LIMIT 1";
 $stmtselect =$db->prepare($sql);
-$result = $stmtselect->execute([]);
+$result = $stmtselect->execute([$username, $password]);
 
 if($result){
-	echo 'success';
+	$user = $stmtselect->fetch(PDO::FETCH_ASSOC);
+	if($stmtselect->rowCount() > 0){
+		echo "1";
+	}else{
+		echo "There no user fot that combo";
+	}
 }else{
 	echo 'THere is an errors';
 }
